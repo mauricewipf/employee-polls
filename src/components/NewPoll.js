143d7f293cf1b585e3298c300f40315fff76b92a
Maurice Wipf <maurice@gmx.de>
2022-06-07 21:44:12 +0200

    feat: Implement NewPoll component

--- src/components/NewPoll.js
+++ src/components/NewPoll.js
@@ -1,13 +1,52 @@
 import {connect} from "react-redux";
+import {useNavigate} from "react-router-dom";
+import {useState} from "react";
 
-const NewPoll = (props) => {
+const NewPoll = ({dispatch}) => {
+    const navigate = useNavigate();
+    const [firstOption, setFirstOption] = useState("");
+    const [secondOption, setSecondOption] = useState("");
+
+    const handleFirstOptionChange = (e) => {
+        const value = e.target.value;
+        setFirstOption(value);
+    };
+
+    const handleSecondOptionChange = (e) => {
+        const value = e.target.value;
+        setSecondOption(value);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        // TODO: dispatch(handleAddPoll(firstOption, secondOption));
+
+        navigate("/");
+    };
+
     return (
         <div>
             <h1>New Poll</h1>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="firstOption">First Option</label>
+                <input
+                    value={firstOption}
+                    onChange={handleFirstOptionChange}
+                    type="text"
+                    id="firstOption"/>
+                <br/>
+                <label htmlFor="secondOption">Second Option</label>
+                <input
+                    value={secondOption}
+                    onChange={handleSecondOptionChange}
+                    type="text"
+                    id="secondOption"/>
+                <br/>
+                <button type="submit">Submit</button>
+            </form>
         </div>
     );
 }
 
-const mapStateToProps = ({}) => ({});
-
-export default connect(mapStateToProps)(NewPoll);
+export default connect()(NewPoll);
