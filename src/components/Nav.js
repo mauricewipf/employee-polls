ccb4b925c614285a8af47dd31482c15221a0e520
Maurice Wipf <maurice@gmx.de>
2022-06-07 21:20:00 +0200

    feat: Implement Nav component

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,15 +1,24 @@
-import {connect} from "react-redux";
+import {Link} from "react-router-dom";
 
-const Nav = (props) => {
+const Nav = () => {
     return (
-        <ul>
-           <li>Dashabord</li>
-           <li>.</li>
-           <li>.</li>
-        </ul>
+        <nav className="nav">
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/leaderboard">Leaderboard</Link>
+                </li>
+                <li>
+                    <Link to="/new">New Tweet</Link>
+                </li>
+                <li>
+                    <Link to="/">Logout</Link>
+                </li>
+            </ul>
+        </nav>
     );
-}
+};
 
-const mapStateToProps = ({}) => ({});
-
-export default connect(mapStateToProps)(Nav);
+export default Nav;
