ebdadc97ce4ca44a25a9144918a5c10e6abe6c62
Maurice Wipf <maurice@gmx.de>
2022-06-08 12:41:36 +0200

    feat: Init required data for Poll page and throw error if user not exists

--- src/components/PollPage.js
+++ src/components/PollPage.js
@@ -1,13 +1,40 @@
 import {connect} from "react-redux";
+import {useParams} from "react-router-dom";
 
-const PollPage = (props) => {
+const PollPage = ({users, questions}) => {
+    const { id } = useParams();
+    const {question, user} = initRequiredData(users, questions, id);
+
     return (
         <div>
-            <h1>Poll</h1>
+            <h1>Poll by {user.id}</h1>
+            <img src={user.avatarURL} alt="Profile image" className="profile-image"/>
+
+            <h2>Would you rather?</h2>
+
+            <p>{question.optionOne.text}</p>
+            <button>Click</button>
+
+            <p>{question.optionTwo.text}</p>
+            <button>Click</button>
         </div>
     );
 }
 
-const mapStateToProps = ({}) => ({});
+const initRequiredData = (users, questions, questionId) => {
+    try {
+        const question = Object.values(questions).find((question) => question.id === questionId);
+        const author = question.author;
+        const user = Object.values(users).find((user) => user.id === author);
+        return {question, user};
+    } catch (e) {
+        throw new Error(`Question or user is not found.\n ${e}`);
+    }
+};
+
+const mapStateToProps = ({users, questions}) => ({
+    users,
+    questions,
+});
 
 export default connect(mapStateToProps)(PollPage);
