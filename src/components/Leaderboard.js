90e16e6f247ed263ca699bcf09a2b7350e12b1dc
Maurice Wipf <maurice@gmx.de>
2022-06-08 23:42:26 +0200

    feat: Display users in leaderboard

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,13 +1,35 @@
 import {connect} from "react-redux";
 
-const Leaderboard = (props) => {
+const Leaderboard = ({users}) => {
     return (
         <div>
             <h1>Leaderboard</h1>
+            <table>
+                <thead>
+                    <tr>
+                        <th>User</th>
+                        <th>Answered</th>
+                        <th>Created</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        users.map((user) => (
+                            <tr key={user.id}>
+                                <td>{user.name} <br/>{user.id}</td>
+                                <td>{Object.keys(user.answers).length}</td>
+                                <td>{user.questions.length}</td>
+                            </tr>
+                        ))
+                    }
+                </tbody>
+            </table>
         </div>
     );
 }
 
-const mapStateToProps = ({}) => ({});
+const mapStateToProps = ({users}) => ({
+    users: Object.values(users),
+});
 
 export default connect(mapStateToProps)(Leaderboard);
