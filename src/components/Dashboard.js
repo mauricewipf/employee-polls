9780c5c8bd9b728d2b43e67eed4af69b81fe22f0
Maurice Wipf <maurice@gmx.de>
2022-06-08 11:08:41 +0200

    feat: Display question on Dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -4,6 +4,28 @@ const Dashboard = (props) => {
     return (
         <div>
             <h1>Dashboard</h1>
+
+            <h2>New Questions</h2>
+            <ul>
+                {Object.values(props.questions)
+                    .filter((question) => true) // TODO
+                    .map((question) => (
+                    <li key={question.id}>
+                        {question.id}
+                    </li>
+                ))}
+            </ul>
+
+            <h2>Answered Questions</h2>
+            <ul>
+                {Object.values(props.questions)
+                    .filter((question) => true) // TODO
+                    .map((question) => (
+                    <li key={question.id}>
+                        {question.id}
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 }
