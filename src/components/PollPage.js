d74451459b65dccb7e50dada083fbdb5fb401c15
Maurice Wipf <maurice@gmx.de>
2022-06-08 23:03:35 +0200

    feat: Highlight voted option and disable button when voted

--- src/components/PollPage.js
+++ src/components/PollPage.js
@@ -1,9 +1,14 @@
 import {connect} from "react-redux";
 import {useNavigate, useParams} from "react-router-dom";
 import {handleAddAnswer} from "../actions/questions";
+import "./PollPage.css";
 
-const PollPage = ({dispatch, user, question}) => {
+const PollPage = ({dispatch, authedUser, question, author}) => {
     const navigate = useNavigate();
+
+    const hasVotedForOptionOne = question.optionOne.votes.includes(authedUser.id);
+    const hasVotedForOptionTwo = question.optionTwo.votes.includes(authedUser.id);
+    const hasVoted = hasVotedForOptionOne || hasVotedForOptionTwo;
 
     const handleOptionOne = (e) => {
         e.preventDefault();
@@ -19,26 +24,29 @@ const PollPage = ({dispatch, user, question}) => {
 
     return (
         <div>
-            <h1>Poll by {user.id}</h1>
-            <img src={user.avatarURL} alt="Profile" className="profile-image"/>
+            <h1>Poll by {author.id}</h1>
+            <img src={author.avatarURL} alt="Profile" className="profile-image"/>
 
             <h2>Would you rather?</h2>
 
-            <p>{question.optionOne.text}</p>
-            <button onClick={handleOptionOne}>Click</button>
+            <div className={hasVotedForOptionOne ? "chosen" : ""}>
+                <p>{question.optionOne.text}</p>
+                <button onClick={handleOptionOne} disabled={hasVoted}>Click</button>
+            </div>
 
-            <p>{question.optionTwo.text}</p>
-            <button onClick={handleOptionTwo}>Click</button>
+            <div className={hasVotedForOptionTwo ? "chosen" : ""}>
+                <p>{question.optionTwo.text}</p>
+                <button onClick={handleOptionTwo} disabled={hasVoted}>Click</button>
+            </div>
         </div>
     );
 }
 
-const mapStateToProps = ({users, questions}) => {
+const mapStateToProps = ({authedUser, users, questions}) => {
     try {
         const question = Object.values(questions).find((question) => question.id === useParams().id);
-        const author = question.author;
-        const user = Object.values(users).find((user) => user.id === author);
-        return {question, user};
+        const author = Object.values(users).find((user) => user.id === question.author);
+        return {authedUser, question, author};
     } catch (e) {
         throw new Error(`Question or user is not found.\n ${e}`);
     }
