4fcf19feb3d23efdf87905156d854e5229cd2b21
Maurice Wipf <maurice@gmx.de>
2022-06-11 00:47:43 +0200

    feat: Style Login

--- src/components/Login.js
+++ src/components/Login.js
@@ -26,23 +26,38 @@ const Login = ({dispatch}) => {
 
     return (
         <div>
-            <h1>Login</h1>
+            <h1 className="text-3xl font-bold mt-9">Login</h1>
             <form onSubmit={handleSubmit}>
-                <label htmlFor="username">Username</label>
-                <input
-                    value={username}
-                    onChange={handleUsername}
-                    type="text"
-                    id="username"/>
-                <br/>
-                <label htmlFor="password">Password</label>
-                <input
-                    value={password}
-                    onChange={handlePassword}
-                    type="password"
-                    id="password"/>
-                <br/>
-                <button type="submit">Login</button>
+                <div>
+                    <label htmlFor="username" className="block text-sm font-medium text-slate-700">Username</label>
+                    <div className="mt-1">
+                        <input
+                            value={username}
+                            onChange={handleUsername}
+                            type="text"
+                            name="username"
+                            id="username"
+                            className="px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400  disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1 invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500 disabled:shadow-none"/>
+                    </div>
+                </div>
+                <div className="mt-6">
+                    <label htmlFor="password" className="block text-sm font-medium text-slate-700">Password</label>
+                    <div className="mt-1">
+                        <input
+                            value={password}
+                            onChange={handlePassword}
+                            type="password"
+                            name="password"
+                            id="password"
+                            className="px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400  disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1 invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500 disabled:shadow-none"/>
+                    </div>
+                </div>
+                <div className="mt-6 text-right">
+                    <button type="submit"
+                        className="bg-sky-500 hover:bg-sky-700 px-5 py-2.5 text-sm leading-5 rounded-md font-semibold text-white">
+                        Login
+                    </button>
+                </div>
             </form>
         </div>
     );
