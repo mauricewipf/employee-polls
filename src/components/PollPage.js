fc0e8fc7db831ff2b12398f31111bc6e518c89da
Maurice Wipf <maurice@gmx.de>
2022-06-11 08:37:16 +0200

    feat: Style PollPage

--- src/components/PollPage.js
+++ src/components/PollPage.js
@@ -36,30 +36,46 @@ const PollPage = ({dispatch, authedUser, question, author}) => {
 
     return (
         <div>
-            <h1>Poll by {author.id}</h1>
-            <img src={author.avatarURL} alt="Profile" className="profile-image"/>
+            <h1 className="text-3xl font-bold mt-9">Poll by {author.id}</h1>
 
-            <h2>Would you rather?</h2>
-
-            <div className={hasVotedForOptionOne ? "chosen" : ""}>
-                <p>{question.optionOne.text}</p>
-                <button onClick={handleOptionOne} disabled={hasVoted}>Click</button>
-                {
-                    hasVoted
-                    ? <p>Votes: {question.optionOne.votes.length} ({calcPercentage("optionOne", question)})</p>
-                    : null
-                }
+            <div className="flex justify-center">
+                <img src={author.avatarURL} alt="Profile" className="h-24 w-24"/>
             </div>
 
-            <div className={hasVotedForOptionTwo ? "chosen" : ""}>
-                <p>{question.optionTwo.text}</p>
-                <button onClick={handleOptionTwo} disabled={hasVoted}>Click</button>
-                {
-                    hasVoted
-                        ? <p>Votes: {question.optionTwo.votes.length} ({calcPercentage("optionTwo", question)})</p>
-                        : null
-                }
+            <div className="flex justify-center">
+                <h2 className="text-2xl font-bold mt-6">Would you rather?</h2>
             </div>
+
+            <div className="grid grid-cols-2 gap-4 mt-4">
+
+                <button onClick={handleOptionOne} disabled={hasVoted}
+                        className={"p-2 rounded-xl bg-zinc-100 hover:shadow-xl transition " + (hasVotedForOptionOne ? "bg-lime-400" : "")}>
+                    <div className={hasVotedForOptionOne ? "chosen" : ""}>
+                        <p className="font-bold mb-2">{question.optionOne.text}</p>
+                        {!hasVoted &&
+                        <p className="underline underline-offset-4 mb-3">Click</p>
+                        }
+                        {hasVoted &&
+                        <p className="text-xs">Votes: {question.optionOne.votes.length} ({calcPercentage("optionOne", question)})</p>
+                        }
+                    </div>
+                </button>
+
+                <button onClick={handleOptionTwo} disabled={hasVoted}
+                        className={"p-2 rounded-xl bg-zinc-100 hover:shadow-xl transition " + (hasVotedForOptionTwo ? "bg-lime-400" : "")}>
+                        <p className="font-bold mb-2">{question.optionTwo.text}</p>
+                        {!hasVoted &&
+                        <p className="underline underline-offset-4 mb-3">Click</p>
+                        }
+                        {hasVoted &&
+                        <p className="text-xs">Votes: {question.optionTwo.votes.length} ({calcPercentage("optionTwo", question)})</p>
+                        }
+                </button>
+
+
+            </div>
+
+
         </div>
     );
 }
