a106cc57b3f6dc404b0f3bb6d4fa1bfd569d76b8
Maurice Wipf <maurice@gmx.de>
2022-06-11 08:09:16 +0200

    feat: Style Leaderboard

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -3,27 +3,31 @@ import {connect} from "react-redux";
 const Leaderboard = ({users}) => {
     return (
         <div>
-            <h1>Leaderboard</h1>
-            <table>
-                <thead>
-                    <tr>
-                        <th>User</th>
-                        <th>Answered</th>
-                        <th>Created</th>
-                    </tr>
+            <h1 className="text-3xl font-bold mt-9">Leaderboard</h1>
+
+            <table className="border-collapse table-auto w-full text-sm mt-6">
+                <thead className="table-header-group">
+                <tr className="table-row">
+                    <th className="border-b dark:border-slate-600 font-medium p-4 pl-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">User</th>
+                    <th className="border-b dark:border-slate-600 font-medium p-4 pl-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">Answered</th>
+                    <th className="border-b dark:border-slate-600 font-medium p-4 pl-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">Created</th>
+                </tr>
                 </thead>
-                <tbody>
-                    {
-                        users.map((user) => (
-                            <tr key={user.id}>
-                                <td>{user.name} <br/>{user.id}</td>
-                                <td>{Object.keys(user.answers).length}</td>
-                                <td>{user.questions.length}</td>
-                            </tr>
-                        ))
-                    }
+                <tbody className="bg-white dark:bg-slate-800">
+                {
+                    users.map((user) => (
+                        <tr key={user.id}>
+                            <td className="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">
+                                <span className="font-bold">{user.name}</span>
+                                <br/>{user.id}</td>
+                            <td className="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">{Object.keys(user.answers).length}</td>
+                            <td className="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">{user.questions.length}</td>
+                        </tr>
+                    ))
+                }
                 </tbody>
             </table>
+
         </div>
     );
 }
