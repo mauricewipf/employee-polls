a5d97bf7443068eb7942820e9e6a9164cc2bbdbf
Maurice Wipf <maurice@gmx.de>
2022-06-16 10:02:30 +0200

    fix: Edit text

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -7,20 +7,22 @@ const Nav = ({dispatch, authedUserId}) => {
     const logout = (e) => {
         e.preventDefault();
         dispatch(handleLogout());
-    }
+    };
 
     return (
         <nav className="flex justify-center space-x-4">
             <Link to="/"
-               className="font-medium px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">Home</Link>
+                  className="font-medium px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">Home</Link>
             <Link to="/leaderboard"
-               className="font-medium px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">Leaderboard</Link>
+                  className="font-medium px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">Leaderboard</Link>
             <Link to="/new"
-               className="font-medium px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">New Tweet</Link>
+                  className="font-medium px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">New
+                Poll</Link>
             <span
-               className="font-medium px-3 py-2 text-slate-700">User: {authedUserId}</span>
+                className="font-medium px-3 py-2 text-slate-700">User: {authedUserId}</span>
             <button onClick={logout}
-                    className="font-medium px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">Logout</button>
+                    className="font-medium px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">Logout
+            </button>
         </nav>
     );
 };
